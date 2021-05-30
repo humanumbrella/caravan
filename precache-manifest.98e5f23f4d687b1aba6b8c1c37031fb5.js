759fc4fcfcc4ce05431c784577a7e25ad68f9eac
Justin Moore <humanumbrella@gmail.com>
2021-05-29 21:39:58 -0700

    Updates

---
+++ precache-manifest.98e5f23f4d687b1aba6b8c1c37031fb5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43c5c5af11ecf17c568c058e8b9d7bfb",
+    "url": "/caravan/index.html"
+  },
+  {
+    "revision": "d6b3cddf68fcf475895d",
+    "url": "/caravan/static/css/2.f2850afe.chunk.css"
+  },
+  {
+    "revision": "f9122418c844d6e8a300",
+    "url": "/caravan/static/css/main.9bf7d482.chunk.css"
+  },
+  {
+    "revision": "d6b3cddf68fcf475895d",
+    "url": "/caravan/static/js/2.892c767a.chunk.js"
+  },
+  {
+    "revision": "0b52b5e0beb68b4f3ea9dd25d3a4440b",
+    "url": "/caravan/static/js/2.892c767a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9122418c844d6e8a300",
+    "url": "/caravan/static/js/main.59805485.chunk.js"
+  },
+  {
+    "revision": "463b8ec66a3715f618ea",
+    "url": "/caravan/static/js/runtime-main.7ddb6876.js"
+  }
+]);
